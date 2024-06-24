91040f4f0d9a3d0ec626ee63dc4f64f2c35e16af
cabelstein12 <Caleb.Steinwandt@gmail.com>
2024-06-23 19:26:06 -0600

    add about me section; begin formatting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+import Image1 from './assets/brittany1.jpeg';
+
+function component(){
+    const container = document.querySelector('.container');
+    const aboutMe = document.querySelector('.about-me-card')
+
+
+    const bioPhoto = new Image();
+    bioPhoto.src = Image1;
+    bioPhoto.style.maxWidth = '200px'
+    aboutMe.prepend(bioPhoto)
+
+    
+
+}
+
+component();
